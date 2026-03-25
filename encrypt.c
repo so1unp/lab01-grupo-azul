d9ef2b1fd1d22cb2f56ce87d7bd770a7321c5a73
SolaNicolas <nicolas.solaleiva@gmail.com>
2026-03-25 15:18:39 -0300

    Test

--- encrypt.c
+++ encrypt.c
@@ -3,7 +3,15 @@
 
 int main(int argc, char *argv[])
 {
+    printf("%d argumentos\n", (argc-1));
+    
+    printf("Debug argv\n");
+    for(int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i+1]);
+    }
+
     // Agregar código aquí.
+    printf("%s argv\n", argv[2]);
 
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
